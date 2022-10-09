a1a42f99c2bd158cd106203fa40d0e685fdc7b4b
Zhafranath <zhafran.athofani23@gmail.com>
2022-10-09 10:20:42 +0700

    menambahkan responsive dan menambahkan if else

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 const btn = document.getElementById("submit");
 btn.addEventListener("click", function() {
-  var result,x,y,z,w;
+  var result,x,y,z,w,v;
   var match = document.getElementById('match').value;
   var winrateA = document.getElementById('winrateA').value;
   var winrateB = document.getElementById('winrateB').value;
@@ -9,6 +9,15 @@ btn.addEventListener("click", function() {
   x = ((100*y)-winrateB*match)/(winrateB-100);
   w = Math.ceil(x);
   console.log(x);
-  z = 'Anda memerlukan sekitar ' + w + ' win tanpa lose untuk mencapai winrate '+ winrateB + '%';
-  result = document.getElementById('hasilB').innerHTML = z;
+  if(x > 0){
+    z = 'Anda memerlukan sekitar ' + w + ' win tanpa lose untuk mencapai winrate '+ winrateB + '%';
+    result = document.getElementById('hasilB').innerHTML = z;
+  }
+  else 
+  {
+    v = Math.abs(w);
+    z = 'Anda memerlukan sekitar ' + v + ' kekalahan untuk mencapai winrate '+ winrateB + '%';
+    result = document.getElementById('hasilB').innerHTML = z;
+  }
+
 });
